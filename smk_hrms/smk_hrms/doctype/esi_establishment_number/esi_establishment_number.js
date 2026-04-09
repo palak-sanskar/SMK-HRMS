d041b48d02ecc7f5cee5e731126ca0525cc739af
krupalirajpara <krupali@sanskartechnolab.com>
2026-04-09 21:21:08 +0530

    feat: create doctype PF and ESI establish number

---
+++ smk_hrms/smk_hrms/doctype/esi_establishment_number/esi_establishment_number.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, jignasha and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("ESI Establishment Number", {
+// 	refresh(frm) {
+
+// 	},
+// });
